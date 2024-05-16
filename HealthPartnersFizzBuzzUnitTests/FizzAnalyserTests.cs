f8df8d2dfe9d5f2d5030745534be1f875655481a
cameron <camskinner@hotmail.co.uk>
2024-05-16 20:27:09 +0100

    Test for divisor to equal 3

--- HealthPartnersFizzBuzzUnitTests/FizzAnalyserTests.cs
+++ HealthPartnersFizzBuzzUnitTests/FizzAnalyserTests.cs
@@ -15,5 +15,11 @@ namespace HealthPartnersFizzBuzzUnitTests
             var output = new FizzAnalyser().Analyse(3);
             Assert.AreEqual("3", output);
         }
+
+        public void FizzAnalyser_Divisor_is_three()
+        {
+            var output = new FizzAnalyser();
+            Assert.AreEqual(3, output.Divisor);
+        }
     }
 }
